109ba041a7e67fd7c61f924966fa6be377ad30c4
Ewie1 <shephvo12@gmail.com>
2023-01-08 14:20:56 +0000

    Add template to comment on post and view comment

--- codestar2023/settings.py
+++ codestar2023/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'cloudinary',
     'blog',
     'django_summernote',
+    'crispy_forms',
 ]
 
 SITE_ID = 1
@@ -58,6 +59,9 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
 ACCOUNT_EMAIL_VERIFICATION = 'none'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
